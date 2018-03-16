9d7c73b34ebc4a29fa6724b7888570fa72bd8321
Calendau GUQUET <kall.guquet@free.fr>
2018-03-16 17:26:13 +0100

    full Rest API

--- Isen.DotNet.Web/Controllers/_BaseApiController.cs
+++ Isen.DotNet.Web/Controllers/_BaseApiController.cs
@@ -19,7 +19,7 @@ namespace Isen.DotNet.Web.Controllers
     {
         [HttpGet]
         [Route("api/[controller]")]
-        public JsonResult GetAll()
+        public virtual JsonResult GetAll()
         {
             var all = _repository
                 .GetAll()
@@ -30,10 +30,44 @@ namespace Isen.DotNet.Web.Controllers
 
         [HttpGet]
         [Route("api/[controller]/{id}")]
-        public JsonResult GetById(int id)
+        public virtual JsonResult GetById(int id)
         {
             var single = _repository.Single(id);
             return Json(single);
+        }
+
+        [HttpPost]
+        [Route("api/[controller]")]
+        public virtual JsonResult Create([FromBody] T model)
+        {
+            _repository.Update(model);
+            _repository.Save();
+            return Json(model.ToDynamic());
+        }
+
+        [HttpPut]
+        [Route("api/[controller]/{id}")]
+        public virtual JsonResult Update(int? id, [FromBody] T model)
+        {
+            if (id == null || model == null || model.Id != id)
+                return Json(null);
+
+            _repository.Update(model);
+            _repository.Save();
+            return Json(model.ToDynamic());
+        }
+
+        [HttpDelete]
+        [Route("api/[controller]/{id}")]
+        public virtual IActionResult Remove(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+            if (_repository.Single(id.Value) == null)
+                return NotFound();
+            _repository.Delete(id.Value);
+            _repository.Save();
+            return new NoContentResult();
         }
     }
 }
